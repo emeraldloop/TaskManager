8d23e4a2d1f0d6809658eca47966d9d617057063
emeraldloop <sergey.nikishin.2000@mail.ru>
2024-02-25 15:28:53 +0300

    методы для SaveChanges

--- src/TaskManager.Domain/Aggregates/Bases/Entities/DomainEntity.cs
+++ src/TaskManager.Domain/Aggregates/Bases/Entities/DomainEntity.cs
@@ -1,6 +1,8 @@
+using TaskManager.Domain.Providers.CurrentTime;
+
 namespace TaskManager.Domain.Aggregates.Bases.Entities;
 
-public class DomainEntity
+public abstract class DomainEntity
     : IAggregateRoot
 {
     public Guid Id { get; }
@@ -12,4 +14,25 @@ public class DomainEntity
     public DateTime? DateDeleted { get; protected set; }
 
     public bool IsDeleted => DateDeleted.HasValue;
+    
+    public DomainEntity Create(ICurrentTimeProvider currentTimeProvider)
+    {
+        DateCreated = currentTimeProvider.GetNow();
+
+        return this;
+    }
+
+    public DomainEntity Update(ICurrentTimeProvider currentTimeProvider)
+    {
+        DateUpdated = currentTimeProvider.GetNow();
+
+        return this;
+    }
+
+    public DomainEntity Delete(ICurrentTimeProvider currentTimeProvider)
+    {
+        DateDeleted = currentTimeProvider.GetNow();
+
+        return this;
+    }
 }
