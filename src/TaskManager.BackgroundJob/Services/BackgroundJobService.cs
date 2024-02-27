e4bbf13c9071c915c0988c69f8046f60b83e9c38
emeraldloop <sergey.nikishin.2000@mail.ru>
2024-02-27 22:35:39 +0300

    кодстайл

--- src/TaskManager.BackgroundJob/Services/BackgroundJobService.cs
+++ src/TaskManager.BackgroundJob/Services/BackgroundJobService.cs
@@ -1,40 +1,30 @@
 namespace TaskManager.BackgroundJob.Services;
 
-public abstract class BackgroundJobService<TJobService>
+public abstract class BackgroundJobService<TJobService>(
+    JobServiceOptions options,
+    IServiceScopeFactory serviceScopeFactory,
+    ILogger<TJobService> logger)
     : BackgroundService
     where TJobService : IJobService
 {
-    private readonly JobServiceOptions _options;
-    private readonly IServiceScopeFactory _serviceScopeFactory;
-    private readonly ILogger<TJobService> _logger;
-
-    protected BackgroundJobService(JobServiceOptions options,
-        IServiceScopeFactory serviceScopeFactory,
-        ILogger<TJobService> logger)
-    {
-        _options = options;
-        _serviceScopeFactory = serviceScopeFactory;
-        _logger = logger;
-    }
-
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
         var serviceName = GetType().Name;
 
-        if (!_options.IsActive)
+        if (!options.IsActive)
         {
-            _logger.LogInformation("Сервис {serviceName} неактивен", serviceName);
+            logger.LogInformation("Сервис {serviceName} неактивен", serviceName);
 
             return;
         }
 
-        _logger.LogInformation("Запуск сервиса {serviceName}", serviceName);
+        logger.LogInformation("Запуск сервиса {serviceName}", serviceName);
 
         while (!cancellationToken.IsCancellationRequested)
         {
             try
             {
-                using var scope = _serviceScopeFactory.CreateScope();
+                using var scope = serviceScopeFactory.CreateScope();
 
                 await scope.ServiceProvider
                     .GetRequiredService<TJobService>()
@@ -43,14 +33,14 @@ public abstract class BackgroundJobService<TJobService>
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Ошибка в {serviceName}", GetType().Name);
+                logger.LogError(ex, "Ошибка в {serviceName}", GetType().Name);
             }
             finally
             {
-                await Task.Delay(_options.ExecutionPeriod, cancellationToken).ConfigureAwait(false);
+                await Task.Delay(options.ExecutionPeriod, cancellationToken).ConfigureAwait(false);
             }
         }
 
-        _logger.LogInformation("Сервис {serviceName} завершил выполнение", serviceName);
+        logger.LogInformation("Сервис {serviceName} завершил выполнение", serviceName);
     }
 }
