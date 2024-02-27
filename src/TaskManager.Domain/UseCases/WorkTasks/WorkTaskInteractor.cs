926fcfe564f0a8552bdebb8b0ba10bd23dc7bf6c
emeraldloop <sergey.nikishin.2000@mail.ru>
2024-02-27 22:19:55 +0300

    кодстайл

--- src/TaskManager.Domain/UseCases/WorkTasks/WorkTaskInteractor.cs
+++ src/TaskManager.Domain/UseCases/WorkTasks/WorkTaskInteractor.cs
@@ -5,44 +5,35 @@ using TaskManager.Domain.Repositories.WorkTasks;
 
 namespace TaskManager.Domain.UseCases.WorkTasks;
 
-public class WorkTaskInteractor
+public class WorkTaskInteractor(
+    ICurrentTimeProvider currentTimeProvider,
+    IWorkTaskRepository workTaskRepository,
+    ISaveRepository saveRepository)
 {
-    private readonly ICurrentTimeProvider _currentTimeProvider;
-    private readonly IWorkTaskRepository _workTaskRepository;
-    private readonly ISaveRepository _saveRepository;
-
-    public WorkTaskInteractor(ICurrentTimeProvider currentTimeProvider,
-        IWorkTaskRepository workTaskRepository, ISaveRepository saveRepository)
-    {
-        _currentTimeProvider = currentTimeProvider;
-        _workTaskRepository = workTaskRepository;
-        _saveRepository = saveRepository;
-    }
-
     public Task<WorkTask> CreateWorkTaskAsync(CancellationToken cancellationToken)
-        => _workTaskRepository.AddAsync(new WorkTask().SetTaskStatus(WorkTaskStatus.Running), cancellationToken);
+        => workTaskRepository.AddAsync(new WorkTask().SetTaskStatus(WorkTaskStatus.Running), cancellationToken);
 
     public Task<WorkTask?> GetWorkTaskNullableAsync(Guid workTaskId, CancellationToken cancellationToken)
-        => _workTaskRepository.GetItemByIdNullableAsync(workTaskId, cancellationToken);
+        => workTaskRepository.GetItemByIdNullableAsync(workTaskId, cancellationToken);
 
     public async Task FinishTasksAsync(TimeSpan workTaskLifeTime, CancellationToken cancellationToken)
     {
-        await foreach (var paginationItem in _workTaskRepository
+        await foreach (var paginationItem in workTaskRepository
                            .GetAsyncEnumerableByFilter(new WorkTaskFilter()
                                    .SetWorkTaskStatus(WorkTaskStatus.Running),
                                cancellationToken))
         {
             foreach (var workTask in paginationItem.PageEntities)
             {
-                if (!workTask.IsTimeToFinish(_currentTimeProvider, workTaskLifeTime))
+                if (!workTask.IsTimeToFinish(currentTimeProvider, workTaskLifeTime))
                 {
                     continue;
                 }
 
-                workTask.FinishTask(_currentTimeProvider);
+                workTask.FinishTask(currentTimeProvider);
             }
 
-            await _saveRepository.SaveChangesAndClearChangeTrackerAsync(cancellationToken).ConfigureAwait(false);
+            await saveRepository.SaveChangesAndClearChangeTrackerAsync(cancellationToken).ConfigureAwait(false);
         }
     }
 }
