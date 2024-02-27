a976790ee8eff41c913866ace36006c5f29ba083
emeraldloop <sergey.nikishin.2000@mail.ru>
2024-02-27 22:19:17 +0300

    кодстайл

--- src/TaskManager.BackgroundJob/Services/WorkTaskFinish/WorkTaskFinishService.cs
+++ src/TaskManager.BackgroundJob/Services/WorkTaskFinish/WorkTaskFinishService.cs
@@ -2,19 +2,11 @@ using TaskManager.Domain.UseCases.WorkTasks;
 
 namespace TaskManager.BackgroundJob.Services.WorkTaskFinish;
 
-public class WorkTaskFinishService
+public class WorkTaskFinishService(
+    WorkTaskInteractor workTaskInteractor,
+    WorkTaskFinishOptions workTaskFinishOptions)
     : IJobService
 {
-    private readonly WorkTaskInteractor _workTaskInteractor;
-    private readonly WorkTaskFinishOptions _workTaskFinishOptions;
-
-    public WorkTaskFinishService(WorkTaskInteractor workTaskInteractor,
-        WorkTaskFinishOptions workTaskFinishOptions)
-    {
-        _workTaskInteractor = workTaskInteractor;
-        _workTaskFinishOptions = workTaskFinishOptions;
-    }
-
     public Task DoJobAsync(CancellationToken cancellationToken)
-        => _workTaskInteractor.FinishTasksAsync(_workTaskFinishOptions.WorkTaskLifeTime, cancellationToken);
+        => workTaskInteractor.FinishTasksAsync(workTaskFinishOptions.WorkTaskLifeTime, cancellationToken);
 }
