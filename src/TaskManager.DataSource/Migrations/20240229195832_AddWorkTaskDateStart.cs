c0cd332abfb244582efba9272cda31e1489cb157
emeraldloop <sergey.nikishin.2000@mail.ru>
2024-02-29 23:11:19 +0300

    миграция добавления даты начала задачи

---
+++ src/TaskManager.DataSource/Migrations/20240229195832_AddWorkTaskDateStart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.DataSource.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorkTaskDateStart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateStart",
+                table: "WorkTasks",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateStart",
+                table: "WorkTasks");
+        }
+    }
+}
